1ef025911455fe6fb6c6164d4a15adb4736293f4
Spenser Galloway <gallosw0@gmail.com>
2020-12-26 18:12:10 -0600

    Create wikispider.js

---
+++ spiderwiki/wikispider.js
@@ -0,0 +1,33 @@
+<script src="/data.js"></script>
+
+<script>
+  function get_fam_by_id(id = -1) {
+    if (!fam_ids.includes(id)) { return null; }
+    var index = ((id >= fams.length) ? fams.length : id);
+
+    // Index should be close to id
+    while (index > 0 && index < fams.length) {
+      if (fams[index][0] == id) { return fams[index]; }
+      if (fams[index][0] > id) {
+        index--;
+      } else {
+        index++;
+      }
+    }
+
+    // Backup search (
+    for(var i = 0; i < fams.length; i++) {
+      if (fams[index][0] == id) { return fams[index]; }
+    }
+    return null;
+  }
+
+  function get_fam_by_name(name = "") {
+    if (name.length < 2) { return null; }
+    name = name.trim().charAt(0).toUpperCase() + name.trim().slice(1).toLowerCase();
+    for(var i = 0; i < fams.length; i++) {
+      if (fams[index][1].localeCompare(name)) { return fams[index]; }
+    }
+    return null;
+  }
+</script>
