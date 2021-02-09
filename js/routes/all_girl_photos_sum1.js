13c3266395ceaf0cf23a42bc4772a48856d08b42
Spenser Galloway <gallosw0@gmail.com>
2021-02-09 17:32:31 -0600

    Update all_girl_photos_sum1.js

--- js/routes/all_girl_photos_sum1.js
+++ js/routes/all_girl_photos_sum1.js
@@ -1,2 +1,19 @@
 get_actions_girl_photos_sum1 = function (a = [], d = 3, g = 300, is_sunny = 1) {
+	var ann_id = get_npc_id('ann');
+	var chicken_id = get_npc_id('chicken');
+	var dog_id = get_npc_id('dog');
+	var duke_id = get_npc_id('bartender');
+	var elli_id = get_npc_id('elli');
+	var horse_id = get_npc_id('horse');
+	var karen_id = get_npc_id('karen');
+	var maria_id = get_npc_id('maria');
+	var mayor_id = get_npc_id('mayor');
+	var popuri_id = get_npc_id('popuri');
+	var rick_id = get_npc_id('rick');
+	var sprite_id = get_npc_id('sprite');
+
+	var dow = get_dow(d, true);
+	var reset = "";
+
+	
 }
