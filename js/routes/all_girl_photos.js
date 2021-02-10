044dbe2a773951143663d2b96100576ef1dc86f7
Spenser Galloway <gallosw0@gmail.com>
2021-02-09 19:01:33 -0600

    Update all_girl_photos.js

--- js/routes/all_girl_photos.js
+++ js/routes/all_girl_photos.js
@@ -10,19 +10,19 @@ get_actions_girl_photos = function (d = 3, g = 300, is_sunny = 1) {
 	}
 
 	if (d <= 30) { // Spring Y1
-		a = actions_girl_photos_spr1(a, d, g, is_sunny);
+		a = get_actions_girl_photos_spr1(a, d, g, is_sunny);
 	}
 	
 	if (d > 30 && d <= 60) { // Summer Y1
-		a = actions_girl_photos_sum1(a, d, g, is_sunny);
+		a = get_actions_girl_photos_sum1(a, d, g, is_sunny);
 	}
 	
 	if (d > 60 && d <= 90) { // Fall Y1
-		a = actions_girl_photos_fall1(a, d, g, is_sunny);
+		a = get_actions_girl_photos_fall1(a, d, g, is_sunny);
 	}
 	
 	if (d > 90 && d <= 120) { // Winter Y1
-		a = actions_girl_photos_win1(a, d, g, is_sunny);
+		a = get_actions_girl_photos_win1(a, d, g, is_sunny);
 	}
 	
 	if (d > 120) { // Spring Y2
