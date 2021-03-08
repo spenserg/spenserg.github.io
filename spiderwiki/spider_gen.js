b4f99b096bc83abaec67cb43e8e3fef8c3d132ef
Spenser Galloway <gallosw0@gmail.com>
2021-03-07 22:32:07 -0600

    Update spider_gen.js

--- spiderwiki/spider_gen.js
+++ spiderwiki/spider_gen.js
@@ -1,17 +1,50 @@
-<script>
-	function parse_gen(str) {
-		var result = [];
+parse_gen=function(str) {
+		var result = {};
 		if (str.includes('familyTitle') && str.includes('scrollToTop')) {
-			str = str.split('familyTitle')[1].split('scrollToTop')[0];
+			str = str.split('ym-wbox')[1].split('scrollToTop')[0];
 			var spec_split = str.split("speciesTitle");
+			var tmp_res = [];
+
+			// Family Info
+			var rx2 = /familyTitle\"\>\s*\<strong\>Family\:([^\<]+)\<\/strong\>([^\<]+)\<span\sclass\=\"lsid\"\>\[urn\:lsid\:nmbe\.ch\:spiderfam\:(\d+)\]\<\/span\>\s+\<a\shref\=\"\/familydetail\/(\d+)\"/g;
+			if (tmp_res = rx2.exec(spec_split[0])) {
+				result['fam_name'] = tmp_res[1].trim();
+				result['fam_auth'] = tmp_res[2].split(",")[0].trim();
+				result['fam_year'] = tmp_res[2].split(",")[1].trim();
+				result['fam_lsid'] = tmp_res[3].trim();
+				result['fam_nmbe_id'] = tmp_res[4].trim();
+			} else { console.log("ERROR: Fam Info Parse Failed"); }
 
 			// Gen Info
-			var rx1 = /genusTitle\"\>Gen\.\s\<strong\>\<i\>([^\<]+)\<\/i\>\<\/strong\>([^\<]+)\<span\sclass\=\"lsid\"\>\[urn\:lsid\:nmbe\.ch\:spidergen\:(\d+)\]/g;
-			var tmp_res = rx1.exec(spec_split[0]);
-			result['name'] = tmp_res[1].trim();
-			result['auth'] = tmp_res[2].trim();
-			result['lsid'] = tmp_res[3].trim();
+			var rx1 = /genusTitle\"\>Gen\.\s\<strong\>\<i\>([^\<]+)\<\/i\>\<\/strong\>([^\<]+)\<span\sclass\=\"lsid\"\>\[urn\:lsid\:nmbe\.ch\:spidergen\:(\d+)\]\<\/span\>\s*\<span\sclass\=\"lsid\"\>\<a\shref\=\"\/genusdetail\/(\d+)\"/g;
+			if (tmp_res = rx1.exec(spec_split[0])) {
+				result['name'] = tmp_res[1].trim();
+				result['auth'] = tmp_res[2].split(",")[0].trim();
+				result['year'] = tmp_res[2].split(",")[1].trim();
+				result['lsid'] = tmp_res[3].trim();
+				result['nmbe_id'] = tmp_res[4].trim();
+			} else { console.log("ERROR: Gen Info Parse Failed"); }
+
+			// Gen Synonyms
+			if (spec_split[0].includes("In synonymy")) {
+				var t_syns = [];
+				if (t_syns = spec_split[0].split("In synonymy:<br>")[1].split("genusTitle")[0].split("<br>")) {
+					var synonyms = [];
+					for (var i = 0; i < (t_syns.length - 1); i++) {
+						rx1 = /\<b\>\<i\>([^\<]+)\<\/i\>\<\/b\>([^\,]+)\,\s*(\d{4})[^\=]+\=[^\=]+\=\"\/genus\/\d+\/[^\"]+\"\>\<b\>\<i\>[^\<]+\<\/i\>\<\/b\>\<\/a\>[^\,]+\,\s*\d{4}\s*\(\<a\shref\=\"\/reference\/(\d+)\"\>[^\<]+\<\/a\>\:*([^\)]+)\)/g
+						if (tmp_res = rx1.exec(t_syns[i])) {
+							synonyms.push({
+								'name':tmp_res[1],
+						 		'auth':tmp_res[2].trim(),
+					 			'year':tmp_res[3].trim(),
+								'ref_id':tmp_res[4],
+								'ref_page':tmp_res[5]
+							});
+						} else { console.log("ERROR: Gen Synonym Info Parse Failed"); }
+					}
+					result['synonyms'] = synonyms;
+				} else { console.log("ERROR: Gen Synonym Parse Failed"); }
+			}
 		}
 		return result;
 	}
-</script>
