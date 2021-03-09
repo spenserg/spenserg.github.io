5bf01b1a0495e8ac59e832618c2c667d7dcb940e
Spenser Galloway <gallosw0@gmail.com>
2021-03-08 19:28:01 -0600

    Update spider_gen.js

--- spiderwiki/spider_gen.js
+++ spiderwiki/spider_gen.js
@@ -25,13 +25,31 @@ parse_gen=function(str) {
 				result['nmbe_id'] = tmp_res[4].trim();
 			} else { console.log("ERROR: Gen Info Parse Failed"); }
 
+			// Gen Transferred
+			if (spec_split[0].includes("Transferred to other")) {
+				rx1 = /\<i\>([^\<]+)\<\/i\>([^\<]+)\s--\ssee\s*\<a\shref\=\"\/genus\/(\d+)\/([^\"]+)\"/g;
+				var xfr = [];
+				var t_split = spec_split[0].split("Transferred to other genera:<br>")[1].split("In synonymy")[0].split("<br>");
+				for (var i = 0; i < (t_split.length - 1); i++) {
+					if (tmp_res = rx1.exec(t_split[i])) {
+						xfr.push({
+							'name':tmp_res[1].trim(),
+							'auth':tmp_res[2].trim(),
+							'xfer_to_name':tmp_res[4],
+							'xfer_to_nmbe_id':tmp_res[3]
+						});
+					} else { console.log("ERROR: Transferred Info Parse Failed"); }
+				}
+				result['transferred'] = xfr;
+			}
+
 			// Gen Synonyms
 			if (spec_split[0].includes("In synonymy")) {
 				var t_syns = [];
 				if (t_syns = spec_split[0].split("In synonymy:<br>")[1].split("genusTitle")[0].split("<br>")) {
 					var synonyms = [];
+					rx1 = /\<b\>\<i\>([^\<]+)\<\/i\>\<\/b\>([^\,]+)\,\s*(\d{4})[^\=]+\=[^\=]+\=\"\/genus\/\d+\/[^\"]+\"\>\<b\>\<i\>[^\<]+\<\/i\>\<\/b\>\<\/a\>[^\,]+\,\s*\d{4}\s*\(\<a\shref\=\"\/reference\/(\d+)\"\>[^\<]+\<\/a\>\:*([^\)]+)\)/g
 					for (var i = 0; i < (t_syns.length - 1); i++) {
-						rx1 = /\<b\>\<i\>([^\<]+)\<\/i\>\<\/b\>([^\,]+)\,\s*(\d{4})[^\=]+\=[^\=]+\=\"\/genus\/\d+\/[^\"]+\"\>\<b\>\<i\>[^\<]+\<\/i\>\<\/b\>\<\/a\>[^\,]+\,\s*\d{4}\s*\(\<a\shref\=\"\/reference\/(\d+)\"\>[^\<]+\<\/a\>\:*([^\)]+)\)/g
 						if (tmp_res = rx1.exec(t_syns[i])) {
 							synonyms.push({
 								'name':tmp_res[1],
