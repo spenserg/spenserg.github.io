d7e9e7ef7acd83e309d8d111263b7f9e4246b03c
Spenser Galloway <gallosw0@gmail.com>
2021-03-12 14:10:53 -0600

    Create spider_func.js

---
+++ spiderwiki/spider_func.js
@@ -0,0 +1,27 @@
+var ucfirst = function (str) {
+	return str.toLowerCase().replace(/^\w/, c => c.toUpperCase());
+}
+
+Array.prototype.unique = function() {
+	return this.filter(function (value, index, self) {
+			return self.indexOf(value) === index;
+		});
+}
+
+String.prototype.replaceAll = function(str1 = "", str2 = "") {
+	var net = 0;
+	var result = this;
+	while (result.includes(str1) && net < 200) {
+		result = result.replace(str1, str2);
+		net++;
+	}
+	return result;
+}
+
+String.prototype.contains = function(valueToFind, fromIndex = null) {
+	console.log('WARNING: Use "INCLUDES" instead of "CONTAINS"');
+	if (fromIndex === null) {
+		return this.includes(valueToFind);
+	}
+	return this.includes(valueToFind, fromIndex);
+}
