667106d54ef364514ae38766b92f24c387d40965
Spenser Galloway <gallosw0@gmail.com>
2025-04-29 18:09:57 -0500

    Update ord_departures.js

--- js/aviation/ord_departures.js
+++ js/aviation/ord_departures.js
@@ -735,7 +735,5 @@ ord_departures = function (arvl = "XXX", tail = null, ac_type = null, result = "
 			result += "<br/>5S<span style=\"color:red\"> (Coord Req)</span>: <input class=\"cdr_input\" style=\"width:75%\" value=\"KORD CMSKY CARYN CYBIL PXV ARG IRW TCC ACH ONM J104 SSO ZONNA2 KTUS\" readonly>"; // Verified with ATCSCC on 02-20-2025
 			break;
 	}
-	document.title = new_title;
-	console.log(new_title);
-	return result;
+	return [result, new_title];
 }
