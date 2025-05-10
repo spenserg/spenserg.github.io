d5f924e2a74aff1661538bb40c4da6d62c6886a2
Spenser Galloway <gallosw0@gmail.com>
2025-05-09 19:19:10 -0500

    las phx

--- js/aviation/get_routes.js
+++ js/aviation/get_routes.js
@@ -3687,15 +3687,14 @@ get_routes = function(dptr = "XXX", arvl = "XXX", tail = null, ac_type = null, r
 					result += "<br/>SP: <input class=\"cdr_input\" style=\"width:75%\" value=\"KLAS GIDGT2 TUKRR DVC LAA J102 SLN J24 MCI J80 SPI J80 VHP ROD J152 JST BOJID4 KPHL\" readonly>";
 					result += "<br/>VU (Ok to File): <input class=\"cdr_input\" style=\"width:75%\" value=\"KLAS NIITZ4 SSKEE INW J86 ELP J50 ABI J4 FUZ UIM ELD IZAAC Q30 VLKNN THRSR TWOUP Q22 BEARI BBDOL PAATS4 KPHL\" readonly>";
 					break;
-				case "PHX": // Updated 04-09-2025
-					result += "<!-- Updated 04-09-2025 -->";
+				case "PHX": // Updated 05-09-2025 // Mandatory Routes checked
+					result += "<!-- Updated 05-09-2025 MP -->";
 					result += "<br/><br/><b>Faa Pref Route (Avg FL290):</b>";
 					result += "<br/><input style=\"width:75%\" value=\"KLAS RASLR3 ZAYNE WOTRO BRUSR1 KPHX\" readonly>";
-					result += "<br/><br/>South: <input style=\"width:75%\" value=\"KLAS RADYR2 LVELL BLH HYDRR1 KPHX\" readonly>";
-					result += "<br/>Low Level Rte: <input style=\"width:75%\" value=\"KLAS DRK COYOT5 KPHX\" readonly>";
+					result += "<br/><br/>West: <input style=\"width:75%\" value=\"KLAS RADYR2 LVELL BLH HYDRR1 KPHX\" readonly>";
 					result += "<br/><br/><b>Non RNAV</b>";
 					result += "<br/><input style=\"width:75%\" value=\"KLAS BLD COYOT5 KPHX\" readonly>";
-					// result += "<br/><br/><b>CDRS</b>"; // No CDRS listed as of 04-09-2025
+					// result += "<br/><br/><b>CDRS</b>"; // No CDRs as of 05-09-2025
 					break;
 			}
 		break; // End of LAS
