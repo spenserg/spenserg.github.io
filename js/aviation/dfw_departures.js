8540cd6d578e3e1047441c62a0d4890b99f097d3
Spenser Galloway <gallosw0@gmail.com>
2025-08-29 14:40:03 -0500

    dfw sdf

--- js/aviation/dfw_departures.js
+++ js/aviation/dfw_departures.js
@@ -1903,15 +1903,18 @@ dfw_departures = function (arvl = "XXX", tail = null, ac_type = null, result = "
 			result += "<br/><input style=\"width:75%\" value=\"KDFW CYOTE4 CNX J74 SJN J231 DRK J6 PMD FIM RZS MQO KSBP\" readonly>";
 			// result += "<br/><br/><b>CDRS</b>";
 			break;
-		case "SDF": // Updated 04-17-2025
-			result += "<!-- Updated 04-17-2025 -->";
+		case "SDF": // Updated 08-29-2025
+			result += "<!-- Updated 08-29-2025 P -->";
 			result += "<br/><br/><b>Faa Pref Route:</b>";
 			result += "<br/><input style=\"width:75%\" value=\"KDFW TRYTN4 LOOSE BUMPY MBELL5 KSDF\" readonly>";
-			result += "<br/><br/>South: <input style=\"width:75%\" value=\"KDFW FORCK3 FORCK ELD SQS BNA EWO KSDF\" readonly>";
+			result += "<br/><br/>North<span style=\"color:red\"> (Coord Req)</span>: <input style=\"width:75%\" value=\"KDFW AKUNA9 MLC MOOCH MBELL5 KSDF\" readonly>"; // CDR 1N
+			result += "<br/>South<span style=\"color:green\"> (Ok to File)</span>: <input style=\"width:75%\" value=\"KDFW FORCK3 FORCK ELD SQS BNA COBBZ LEDDL1 KSDF\" readonly>";
+			result += "<br/><br/>North via STL<span style=\"color:green\"> (Ok to File)</span>: <input style=\"width:75%\" value=\"KDFW AKUNA9 MLC RZC STL JMONY JMONY2 KSDF\" readonly>";
+			result += "<br/>South via VUZ<span style=\"color:green\"> (Ok to File)</span>: <input style=\"width:75%\" value=\"KDFW FORCK3 FORCK ELD SQS VUZ GQO GLAZR LEDDL1 KSDF\" readonly>";
 			result += "<br/><br/><b>Non RNAV</b>";
 			result += "<br/><input style=\"width:75%\" value=\"KDFW DALL3 LIT J131 PXV KSDF\" readonly>";
-			result += "<br/><br/><b>CDRS</b>"; // Verified with ATCSCC on 04-17-2025
-			result += "<!-- Verified with ATCSCC on 04-17-2025 -->";
+			result += "<br/><br/><b>CDRS</b>"; // Verified with ATCSCC on 08-29-2025
+			result += "<!-- Verified with ATCSCC on 08-29-2025 -->";
 			result += "<br/>0P<span style=\"color:green\"> (Ok to File)</span>: <input class=\"cdr_input\" style=\"width:75%\" value=\"KDFW ZACHH4 BSKAT LIT MOOCH MBELL5 KSDF\" readonly>";
 			result += "<br/>1N<span style=\"color:red\"> (Coord Req)</span>: <input class=\"cdr_input\" style=\"width:75%\" value=\"KDFW AKUNA9 MLC MOOCH MBELL5 KSDF\" readonly>";
 			result += "<br/>1S<span style=\"color:red\"> (Coord Req)</span>: <input class=\"cdr_input\" style=\"width:75%\" value=\"KDFW DARTZ9 TNV IAH LCH J22 MCB J35 MEM BUMPY MBELL5 KSDF\" readonly>";
