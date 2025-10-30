99b33322ce468cd8e241b506f7b3d6aefe67c3a7
Spenser Galloway <gallosw0@gmail.com>
2025-10-30 17:06:26 -0500

    Update SEA route inputs in mia_departures.js

--- js/aviation/mia_departures.js
+++ js/aviation/mia_departures.js
@@ -953,7 +953,7 @@ mia_departures = function (arvl = "XXX", tail = null, ac_type = null, result = "
 		case "SEA": // MIA-SEA // Updated 05-31-2025
 			result += "<!-- Updated 05-31-2025 -->";
 			// result += "<br/><br/><b>Faa Pref Route:</b>";
-			result += "<br/><br/>OW: <input style=\"width:75%\" value=\"KMIA GLADZ4 BAGGS Y290 BLVNS Q105 HRV J58 SPS HEATR Q154 HOVEL BKE PDT CHINS5 KSEA\" readonly>";
+			result += "<br/><br/>OW: <input style=\"width:75%\" value=\"KMIA GLADZ4 BAGGS REMIS Y280 REDFN Q105 HRV J58 SPS NOSEW Q154 HOVEL BKE PDT CHINS5 KSEA\" readonly>";
 			result += "<br/><br/>Non OW: <input style=\"width:75%\" value=\"KMIA HURCN4 SMELZ Q116 JAWJA CABLO DEFUN J2 CEW J50 MCB EIC J20 IRW J98 MMB LAA J52 OCS J20 PDT CHINS5 KSEA\" readonly>"; // CDR G0
 			result += "<br/>North: <input style=\"width:75%\" value=\"KMIA HURCN4 SMELZ Q116 VLKNN GETME J151 FAM IRK J151 BIL J136 MLP GLASR3 KSEA\" readonly>";
 			result += "<br/>South: <input style=\"width:75%\"; value=\"KMIA GLADZ4 BAGGS REMIS Y280 LEV J86 BAVPE CORKR Q35 IMB YKM CHINS5 KSEA\" readonly>";
