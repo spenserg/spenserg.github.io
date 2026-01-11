180a16795aaaacd465432371a91e26781b7aab70
Spenser Galloway <gallosw0@gmail.com>
2026-01-11 15:00:54 -0600

    mia-eyw

--- js/aviation/mia_departures.js
+++ js/aviation/mia_departures.js
@@ -391,7 +391,7 @@ mia_departures = function (arvl = "XXX", tail = null, ac_type = null, result = "
 			break;
 		case "EYW": // MIA-EYW // Updated 01-10-2026
 			result += "<!-- Updated 01-10-2026 P -->";
-			result += "<br/><br/><b>Faa Pref Route:</b>";
+			result += "<br/><br/><b>Faa Pref Route (Avg FL180):</b>";
 			result += "<br/><input style=\"width:75%\" value=\"KMIA MAYNR3 MAYNR FNTSY FNTSY1 KEYW\" readonly>";
 			result += "<br/></br>North<span style=\"color:red\"> (Coord Req)</span>: <input style=\"width:75%\" value=\"KMIA GLADZ4 GLADZ KARTR KRAKN1 KEYW\" readonly>"; // CDR G2
 			// result += "<br/><br/><b>Non RNAV</b>";
