a82986ffbb51bb66430abf085e6e3616edfb3c5b
Spenser Galloway <gallosw0@gmail.com>
2026-01-11 19:36:11 -0600

    clt gsp

--- js/aviation/clt_departures.js
+++ js/aviation/clt_departures.js
@@ -523,13 +523,13 @@ clt_departures = function (arvl = "XXX", tail = null, ac_type = null, result = "
 			result += "<br/><br/><b>Faa Pref Route (Avg FL090):</b>";
 			result += "<br/><input style=\"width:75%\" value=\"KCLT CLT3 GANTS KGSO\" readonly>";
 			break;
-		case "GSP": // CLT-GSP // Updated 02-06-2025
-			result += "<!-- Updated 02-06-2025 -->";
+		case "GSP": // CLT-GSP // Updated 01-11-2026
+			result += "<!-- Updated 01-11-2026 P -->";
 			result += "<br/><br/><b>Faa Pref Route (Avg FL100):</b>";
 			result += "<br/><input style=\"width:75%\" value=\"KCLT BOBZY5 BOBZY SPA KGSP\" readonly>";
 			result += "<br/><br/><b>Non RNAV</b>";
 			result += "<br/><input style=\"width:75%\" value=\"KCLT CLT3 HARAY SPA KGSP\" readonly>";
-			// result += "<br/><br/><b>CDRS</b>";
+			// result += "<br/><br/><b>CDRS</b>"; // No CDRs as of 01-11-2026
 			break;
 		case "HSV": // CLT-HSV // Updated 02-06-2025
 			result += "<!-- Updated 02-06-2025 -->";
